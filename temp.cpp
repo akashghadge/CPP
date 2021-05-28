6a46e1dca7a96344f52f7b2f616e8ae11ab02f1d
akash_ghadge <asghadge6@gmail.com>
2021-05-28 22:10:42 +0530

    2D Array All 30 Que completed

--- temp.cpp
+++ temp.cpp
@@ -1,27 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#include <unordered_map>
 int main()
 {
-    int n;
-    cin >> n;
-    string str[n];
-    int i = 0;
-    while (i < n)
-    {
-        cin >> str[i];
-        i++;
-    }
-    unordered_map<char, int> mp;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < str[i].size(); j++)
-        {
-            mp[str[i][j]]++;
-        }
-    }
-    for (auto val : mp)
-    {
-        cout << val.first << " is " << val.second << " times" << endl;
-    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "hello world" << endl;
 }

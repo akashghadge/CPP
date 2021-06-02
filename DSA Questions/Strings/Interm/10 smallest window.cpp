35aa05630c9d4adf2e45e8420c88c4648ac85a89
akash_ghadge <asghadge6@gmail.com>
2021-06-02 16:48:23 +0530

    String Interm 1 Que added

---
+++ DSA Questions/Strings/Interm/10 smallest window.cpp
@@ -0,0 +1,98 @@
+/*
+Given a string 's'. The task is to find the smallest window length that contains all the characters of the given string at least one time.
+For eg. A = “aabcbcdbca”, then the result would be 4 as of the smallest window will be “dbca”.
+
+ 
+
+Example 1:
+
+Input : "AABBBCBBAC"
+Output : 3
+Explanation : Sub-string -> "BAC"
+Example 2:
+Input : "aaab"
+Output : 2
+Explanation : Sub-string -> "ab"
+ 
+Example 3:
+Input : "GEEKSGEEKSFOR"
+Output : 8
+Explanation : Sub-string -> "GEEKSFOR"
+ 
+
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function findSubString() which takes the string  S as inputs and returns the length of the smallest such string.
+
+
+Expected Time Complexity: O(256.N)
+Expected Auxiliary Space: O(256)
+
+ 
+
+Constraints:
+1 ≤ |S| ≤ 105
+String may contain both type of English Alphabets.
+
+
+*/
+
+#include <bits/stdc++.h>
+#define en "\n"
+using namespace std;
+class Solution
+{
+public:
+    // here we use concept sliding window and hashing
+    string findSubString(string str)
+    {
+        // Your code goes here
+        // firstly we store all distinct element
+        unordered_map<char, int> freq;
+        for (char val : str)
+        {
+            freq[val]++;
+        }
+        // max distinct eleement
+        int disEle = freq.size();
+        int currCount = 0;
+        freq.clear();
+        int start = 0;
+
+        int minLen = INT_MAX;
+        int start_index = -1;
+        for (int i = 0; i < str.size(); i++)
+        {
+            freq[str[i]]++;
+            // if distinct ele occure we cnt++
+            if (freq[str[i]] == 1)
+            {
+                currCount++;
+            }
+            // if we get all elements then just shrink window
+            if (currCount == disEle)
+            {
+                // while shrinking window we check first charecter is duplicate or not
+                while (freq[str[start]] > 1)
+                {
+                    freq[str[start]]--;
+                    start++;
+                }
+                // Update window size
+                int len_window = i - start + 1;
+                if (minLen > len_window)
+                {
+                    minLen = len_window;
+                    start_index = start;
+                }
+            }
+        }
+        return str.substr(start_index, minLen);
+    }
+};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}

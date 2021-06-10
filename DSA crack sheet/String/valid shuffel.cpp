fc3270eddac62433d3be32012d96bc2fa9726d50
akash_ghadge <asghadge6@gmail.com>
2021-06-10 12:54:07 +0530

    DSA Sheet String 3 que added

---
+++ DSA crack sheet/String/valid shuffel.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define en "\n"
+using namespace std;
+bool sol(string str1, string str2, string res)
+{
+    int n, m, t;
+    n = str1.size();
+    m = str2.size();
+    t = res.size();
+    int i = 0, j = 0, k = 0;
+    if (n + m != t)
+        return false;
+    while (k < t)
+    {
+        if (i < n && str1[i] == res[k])
+            i++;
+        else if (j < m && str2[j] == res[k])
+            j++;
+        else
+            return false;
+        k++;
+    }
+    if (i < n || j < m)
+        return false;
+    return true;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string str1, str2, res;
+    cin >> str1 >> str2 >> res;
+    cout << sol(str1, str2, res) << endl;
+    return 0;
+}

5e7d14baedcbf54685296b085057da027fb7c84c
akash_ghadge <asghadge6@gmail.com>
2021-07-03 22:55:39 +0530

    CodeChef July 21 1 que completed

--- temp.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>>
-void fun() { printf("I am from fun"); }
-void dun() { printf("I am from dun"); }
-void main()
-{
-    int xxx[10] = {5};
-    printf("%d %d", xxx[1], xxx[9]);
-}
---
+++ temp.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define en "\n"
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << ceil(1 / 3);
+    return 0;
+}

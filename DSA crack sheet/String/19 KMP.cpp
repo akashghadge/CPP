b91054d7699ace3b3cc25e88a9de9ffd482d7972
akash_ghadge <asghadge6@gmail.com>
2021-07-03 16:11:29 +0530

    DSA sheet String 35 all completed

--- DSA crack sheet/String/19 KMP.cpp
+++ DSA crack sheet/String/19 KMP.cpp
@@ -33,7 +33,7 @@ using namespace std;
 void printVector(vector<int> vec)
 {
     for (int i = 0; i < vec.size(); i++)
-    {
+    { 
         cout << "At Index " << i << " Vector have : " << vec[i] << endl;
     }
     cout << endl;

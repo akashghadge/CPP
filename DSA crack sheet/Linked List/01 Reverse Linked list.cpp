2101c4a104b8b2d59430cd53d5560f0f01d13c6a
akash_ghadge <asghadge6@gmail.com>
2021-07-21 11:47:32 +0530

    LINKED LIST STARTED

---
+++ DSA crack sheet/Linked List/01 Reverse Linked list.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+int main()
+{
+    FAST;
+
+    return 0;
+}

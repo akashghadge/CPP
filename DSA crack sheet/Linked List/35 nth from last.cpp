0a8ad233e171469af541903c45e8214d0e167de8
akash_ghadge <asghadge6@gmail.com>
2021-07-27 11:45:57 +0530

    DSA sheet linklist 1 added

---
+++ DSA crack sheet/Linked List/35 nth from last.cpp
@@ -0,0 +1,84 @@
+/*
+Given a linked list consisting of L nodes and given a number N. The task is to find the Nth node from the end of the linked list.
+
+Example 1:
+
+Input:
+N = 2
+LinkedList: 1->2->3->4->5->6->7->8->9
+Output: 8
+Explanation: In the first example, there
+are 9 nodes in linked list and we need
+to find 2nd node from end. 2nd node
+from end os 8.  
+Example 2:
+
+Input:
+N = 5
+LinkedList: 10->5->100->5
+Output: -1
+Explanation: In the second example, there
+are 4 nodes in the linked list and we
+need to find 5th from the end. Since 'n'
+is more than the number of nodes in the
+linked list, the output is -1.
+Your Task:
+The task is to complete the function getNthFromLast() which takes two arguments: reference to head and N and you need to return Nth from the end or -1 in case node doesn't exist..
+
+Note:
+Try to solve in single traversal.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= L <= 106
+1 <= N <= 106
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+//Function to find the data of nth node from the end of a linked list.
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+int getNthFromLast(struct Node *head, int n)
+{
+    int size = 0;
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        size++;
+    }
+    size -= n;
+    if (size < 0)
+        return -1;
+
+    temp = head;
+    int curr = 0;
+    while (curr < size)
+    {
+        temp = temp->next;
+        curr++;
+    }
+    return temp->data;
+}
+int main()
+{
+    FAST;
+
+    return 0;
+}

0663215976e25c7ea9e56c9d6b30e4faff038ce6
akash_ghadge <asghadge6@gmail.com>
2021-07-28 12:36:54 +0530

    DSA linklist GFG 1 added

---
+++ DSA Questions/linked list que/GFG/merge sorted list.cpp
@@ -0,0 +1,76 @@
+/*
+Given two sorted linked lists consisting of N and M nodes respectively. The task is to merge both of the list (in-place) and return head of the merged list.
+ 
+
+Example 1:
+
+Input:
+N = 4, M = 3 
+valueN[] = {5,10,15,40}
+valueM[] = {2,3,20}
+Output: 2 3 5 10 15 20 40
+Explanation: After merging the two linked
+lists, we have merged list as 2, 3, 5,
+10, 15, 20, 40.
+Example 2:
+
+Input:
+N = 2, M = 2
+valueN[] = {1,1}
+valueM[] = {2,4}
+Output:1 1 2 4
+Explanation: After merging the given two
+linked list , we have 1, 1, 2, 4 as
+output.
+Your Task:
+The task is to complete the function sortedMerge() which takes references to the heads of two linked lists as the arguments and returns the head of merged linked list.
+
+Expected Time Complexity : O(n+m)
+Expected Auxilliary Space : O(1)
+
+Constraints:
+1 <= N, M <= 104
+1 <= Node's data <= 105
+
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+struct Node *SortedMerge(struct Node *a, struct Node *b)
+{
+    if (!a)
+        return b;
+    if (!b)
+        return a;
+    struct Node *res = NULL;
+    if (a->data <= b->data)
+    {
+        res = a;
+        res->next = SortedMerge(a->next, b);
+    }
+    else
+    {
+        res = b;
+        res->next = SortedMerge(a, b->next);
+    }
+    return res;
+}
+int main()
+{
+    FAST;
+
+    return 0;
+}

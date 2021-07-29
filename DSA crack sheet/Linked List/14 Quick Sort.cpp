4527e7e6319cbf7db5a77c4f8cfa56258180aabb
akash_ghadge <asghadge6@gmail.com>
2021-07-29 11:47:08 +0530

    DSA sheet linklist 1 added

--- DSA crack sheet/Linked List/14 Quick Sort.cpp
+++ DSA crack sheet/Linked List/14 Quick Sort.cpp
@@ -1,87 +1,164 @@
-/*
-Sort the given Linked List using quicksort. which takes O(n^2) time in worst case and O(nLogn) in average and best cases, otherwise you may get TLE.
-
-Input:
-In this problem, method takes 1 argument: address of the head of the linked list. The function should not read any input from stdin/console.
-The struct Node has a data part which stores the data and a next pointer which points to the next element of the linked list.
-There are multiple test cases. For each test case, this method will be called individually.
-
-Output:
-Set *headRef to head of resultant linked list.
-
-User Task:
-The task is to complete the function quickSort() which should set the *headRef to head of the resultant linked list.
-
-Constraints:
-1<=T<=100
-1<=N<=200
-
-Note: If you use "Test" or "Expected Output Button" use below example format
-
-Example:
-Input:
-2
-3
-1 6 2
-4
-1 9 3 8
-
-Output:
-1 2 6
-1 3 8 9
-
-Explanation:
-Testcase 1: After sorting the nodes, we have 1, 2 and 6.
-Testcase 2: After sorting the nodes, we have 1, 3, 8 and 9.
-
-*/
-
-#include <bits/stdc++.h>
+// C++ program for Quick Sort on Singly Linled List
+#include <cstdio>
+#include <iostream>
 using namespace std;
-#define en "\n"
-#define vi vector<int>
-#define vll vector<ll>
-#define ll long long
-#define FAST                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-struct node
+
+/* a node of the singly linked list */
+struct Node
 {
     int data;
-    struct node *next;
-
-    node(int x)
-    {
-        data = x;
-        next = NULL;
-    }
+    struct Node *next;
 };
 
-//you have to complete this function
-node *part(node *head, node *tail)
+/* A utility function to insert a node at the beginning of
+* linked list */
+void push(struct Node **head_ref, int new_data)
 {
-    
+    /* allocate node */
+    struct Node *new_node = new Node;
+
+    /* put in the data */
+    new_node->data = new_data;
+
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+
+    /* move the head to point to the new node */
+    (*head_ref) = new_node;
 }
-node *quick(node *head, node *tail)
+
+/* A utility function to print linked list */
+void printList(struct Node *node)
 {
-    node *pivot = part(head, tail);
-    quick(head, pivot);
-    quick(pivot, tail);
-}
-void quickSort(struct node **headRef)
-{
-    node *head = *headRef;
-    node *tail = *headRef;
-    while (!tail->next)
+    while (node != NULL)
     {
-        tail = tail->next;
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+// Returns the last node of the list
+struct Node *getTail(struct Node *cur)
+{
+    while (cur != NULL && cur->next != NULL)
+        cur = cur->next;
+    return cur;
+}
+
+// Partitions the list taking the last element as the pivot
+struct Node *partition(struct Node *head, struct Node *end,
+                       struct Node **newHead,
+                       struct Node **newEnd)
+{
+    struct Node *pivot = end;
+    struct Node *prev = NULL, *cur = head, *tail = pivot;
+
+    // During partition, both the head and end of the list
+    // might change which is updated in the newHead and
+    // newEnd variables
+    while (cur != pivot)
+    {
+        if (cur->data < pivot->data)
+        {
+            // First node that has a value less than the
+            // pivot - becomes the new head
+            if ((*newHead) == NULL)
+                (*newHead) = cur;
+
+            prev = cur;
+            cur = cur->next;
+        }
+        else // If cur node is greater than pivot
+        {
+            // Move cur node to next of tail, and change
+            // tail
+            if (prev)
+                prev->next = cur->next;
+            struct Node *tmp = cur->next;
+            cur->next = NULL;
+            tail->next = cur;
+            tail = cur;
+            cur = tmp;
+        }
     }
 
-    *headRef = quick(head, tail);
+    // If the pivot data is the smallest element in the
+    // current list, pivot becomes the head
+    if ((*newHead) == NULL)
+        (*newHead) = pivot;
+
+    // Update newEnd to the current last node
+    (*newEnd) = tail;
+
+    // Return the pivot node
+    return pivot;
 }
+
+// here the sorting happens exclusive of the end node
+struct Node *quickSortRecur(struct Node *head,
+                            struct Node *end)
+{
+    // base condition
+    if (!head || head == end)
+        return head;
+
+    Node *newHead = NULL, *newEnd = NULL;
+
+    // Partition the list, newHead and newEnd will be
+    // updated by the partition function
+    struct Node *pivot = partition(head, end, &newHead, &newEnd);
+
+    // If pivot is the smallest element - no need to recur
+    // for the left part.
+    if (newHead != pivot)
+    {
+        // Set the node before the pivot node as NULL
+        struct Node *tmp = newHead;
+        while (tmp->next != pivot)
+            tmp = tmp->next;
+        tmp->next = NULL;
+
+        // Recur for the list before pivot
+        newHead = quickSortRecur(newHead, tmp);
+
+        // Change next of last node of the left half to
+        // pivot
+        tmp = getTail(newHead);
+        tmp->next = pivot;
+    }
+
+    // Recur for the list after the pivot element
+    pivot->next = quickSortRecur(pivot->next, newEnd);
+
+    return newHead;
+}
+
+// The main function for quick sort. This is a wrapper over
+// recursive function quickSortRecur()
+void quickSort(struct Node **headRef)
+{
+    (*headRef) = quickSortRecur(*headRef, getTail(*headRef));
+    return;
+}
+
+// Driver code
 int main()
 {
-    FAST;
+    struct Node *a = NULL;
+    push(&a, 5);
+    push(&a, 20);
+    push(&a, 4);
+    push(&a, 3);
+    push(&a, 30);
+
+    cout << "Linked List before sorting \n";
+    printList(a);
+
+    quickSort(&a);
+
+    cout << "Linked List after sorting \n";
+    printList(a);
 
     return 0;
 }

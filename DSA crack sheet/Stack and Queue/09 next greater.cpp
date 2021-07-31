8fc7d9c535b773f3ed5eb6454684080c9496c762
akash_ghadge <asghadge6@gmail.com>
2021-07-31 13:37:25 +0530

    DSA sheet sq 1 added

---
+++ DSA crack sheet/Stack and Queue/09 next greater.cpp
@@ -0,0 +1,80 @@
+/*
+Given an array arr[ ] of size N having distinct elements, the task is to find the next greater element for each element of the array in order of their appearance in the array.
+Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+If there does not exist next greater of current element, then next greater element for current element is -1. For example, next greater of the last element is always -1.
+
+Example 1:
+
+Input: 
+N = 4, arr[] = [1 3 2 4]
+Output:
+3 4 4 -1
+Explanation:
+In the array, the next larger element 
+to 1 is 3 , 3 is 4 , 2 is 4 and for 4 ? 
+since it doesn't exist, it is -1.
+Example 2:
+
+Input: 
+N = 5, arr[] [6 8 0 1 3]
+Output:
+8 -1 1 3 -1
+Explanation:
+In the array, the next larger element to 
+6 is 8, for 8 there is no larger elements 
+hence it is -1, for 0 it is 1 , for 1 it 
+is 3 and then for 3 there is no larger 
+element on right and hence -1.
+Your Task:
+This is a function problem. You only need to complete the function nextLargerElement() that takes list of integers arr[ ] and N as input parameters and returns list of integers of length N denoting the next greater elements for all the corresponding elements in the input array.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(N)
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ Ai ≤ 1018
+
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+vector<long long> nextLargerElement(vector<long long> arr, int n)
+{
+    // Your code here
+    vector<long long> output(n, 0);
+
+    stack<long long> s;
+
+    s.push(arr[n - 1]);
+    output[n - 1] = -1;
+
+    for (long long i = n - 2; i >= 0; i--)
+    {
+        while (s.empty() == false && s.top() <= arr[i])
+        {
+            s.pop();
+        }
+
+        long long ng = s.empty() ? (-1) : s.top();
+
+        output[i] = ng;
+
+        s.push(arr[i]);
+    }
+
+    return output;
+}
+int main()
+{
+    FAST;
+
+    return 0;
+}

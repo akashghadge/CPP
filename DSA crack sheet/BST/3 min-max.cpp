bab036bc67ab39cdfa423cc4766e7c0eb7d9428e
akash_ghadge <asghadge6@gmail.com>
2021-08-28 14:14:56 +0530

    DSA SHEET BST STARTED

---
+++ DSA crack sheet/BST/3 min-max.cpp
@@ -0,0 +1,71 @@
+/*
+Given a Binary Search Tree. The task is to find the minimum element in this given BST.
+
+Example 1:
+
+Input:
+           5
+         /    \
+        4      6
+       /        \
+      3          7
+     /
+    1
+Output: 1
+Example 2:
+
+Input:
+             9
+              \
+               10
+                \
+                 11
+Output: 9
+Your Task:
+The task is to complete the function minValue() which takes root as the argument and returns the minimum element of BST. If the tree is empty, there is no minimum elemnt, so retutn -1 in that case.
+
+Expected Time Complexity: O(Height of the BST)
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+1 <= N <= 104
+
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int newData)
+    {
+        data = newData;
+        left = right = NULL;
+    }
+};
+//Function to find the minimum element in the given BST.
+int minValue(Node *root)
+{
+    // Code here
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root->data;
+}
+
+int main()
+{
+    FAST;
+
+    return 0;
+}
---
+++ DSA crack sheet/BST/4 Predecessor and Successor.cpp
@@ -0,0 +1,140 @@
+/*
+There is BST given with root node with key part as integer only. You need to find the inorder successor and predecessor of a given key. In case, if the either of predecessor or successor is not found print -1.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains n denoting the number of edges of the BST. The next line contains the edges of the BST. The last line contains the key.
+
+Output:
+Print the predecessor followed by successor for the given key. If the predecessor or successor is not found print -1.
+
+Constraints:
+1<=T<=100
+1<=n<=100
+1<=data of node<=100
+1<=key<=100
+
+Example:
+Input:
+2
+6
+50 30 L 30 20 L 30 40 R 50 70 R 70 60 L 70 80 R
+65
+6
+50 30 L 30 20 L 30 40 R 50 70 R 70 60 L 70 80 R
+100
+
+Output:
+60 70
+80 -1
+
+*/
+// C++ program to find predecessor and successor in a BST
+#include <iostream>
+using namespace std;
+
+// BST Node
+struct Node
+{
+    int key;
+    struct Node *left, *right;
+};
+
+// This function finds predecessor and successor of key in BST.
+// It sets pre and suc as predecessor and successor respectively
+void findPreSuc(Node *root, Node *&pre, Node *&suc, int key)
+{
+    // Base case
+    if (root == NULL)
+        return;
+
+    // If key is present at root
+    if (root->key == key)
+    {
+        // the maximum value in left subtree is predecessor
+        if (root->left != NULL)
+        {
+            Node *tmp = root->left;
+            while (tmp->right)
+                tmp = tmp->right;
+            pre = tmp;
+        }
+
+        // the minimum value in right subtree is successor
+        if (root->right != NULL)
+        {
+            Node *tmp = root->right;
+            while (tmp->left)
+                tmp = tmp->left;
+            suc = tmp;
+        }
+        return;
+    }
+
+    // If key is smaller than root's key, go to left subtree
+    if (root->key > key)
+    {
+        suc = root;
+        findPreSuc(root->left, pre, suc, key);
+    }
+    else // go to right subtree
+    {
+        pre = root;
+        findPreSuc(root->right, pre, suc, key);
+    }
+}
+
+// A utility function to create a new BST node
+Node *newNode(int item)
+{
+    Node *temp = new Node;
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+/* A utility function to insert a new node with given key in BST */
+Node *insert(Node *node, int key)
+{
+    if (node == NULL)
+        return newNode(key);
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else
+        node->right = insert(node->right, key);
+    return node;
+}
+
+// Driver program to test above function
+int main()
+{
+    int key = 65; //Key to be searched in BST
+
+    /* Let us create following BST
+			50
+		/	 \
+		30	 70
+		/ \ / \
+	20 40 60 80 */
+    Node *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    Node *pre = NULL, *suc = NULL;
+
+    findPreSuc(root, pre, suc, key);
+    if (pre != NULL)
+        cout << "Predecessor is " << pre->key << endl;
+    else
+        cout << "No Predecessor";
+
+    if (suc != NULL)
+        cout << "Successor is " << suc->key;
+    else
+        cout << "No Successor";
+    return 0;
+}

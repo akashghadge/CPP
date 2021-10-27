c14780d698bdb689339d1058b633ae9509d89599
akash_ghadge <asghadge6@gmail.com>
2021-10-27 20:12:40 +0530

    DSA sheet greedy 2 adde

--- DSA crack sheet/Greedy/29 choclate choco.cpp
+++ DSA crack sheet/Greedy/29 choclate choco.cpp
@@ -29,67 +29,93 @@ Notice below used sort method, sending greater() as 3rd argument to sort method 
 */
 
 // C++ program to divide a board into m*n squares
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-// method returns minimum cost to break board into
-// m*n squares
-int minimumCostOfBreaking(int X[], int Y[], int m, int n)
+/* SPOJ: CHOCOLA
+Idea: Greedy Algorithm Stays Ahead
+The number of breaks needed is always the same.
+#Proof:
+C(n,m) = n + m + n*m
+C(0,0) = 0 (a single box choclate needs 0 cut)
+Cut horizontally:
+C(n,m) = 1 + C(a,m) + C(b,m) such that a+b+1 = n
+C(n,m) = 1 + a+m+a*m + b+m+b*m
+       = 1 + (n-1) + 2*m + (n-1)*m
+       = n + m + n*m   :)
+equally for vertical cut
+#
+Observation: the problem is similar to weight distribution problem
+Observation: each cut will happen sooner or later
+Observation: the "weight" of a cut is non decreasing
+Observation: if a vertical and horizontal cost are equal, cutting 
+order does not matter. Proof?
+So we greedily choose the largest cut-cost each time
+*/
+
+int x[1003], y[1003];
+
+int main()
 {
-    sort(X, X + m, greater<int>());
-    sort(Y, Y + n, greater<int>());
-    int h_cuts = 1;
-    int v_cuts = 1;
-    int i = 0, j = 0;
-    int ans = 0;
-    while (i < m && j < n)
+    int tc;
+    cin >> tc;
+    while (tc--)
     {
-        if (X[i] > Y[j])
+        int m, n;
+        cin >> m >> n;
+        for (int i = 1; i < m; ++i)
         {
-            ans += (X[i] * v_cuts);
-            h_cuts++;
-            i++;
+            cin >> x[i];
+        }
+        for (int i = 1; i < n; ++i)
+        {
+            cin >> y[i];
+        }
+        int h = 1, v = 1;
+        sort(x + 1, x + m);
+        reverse(x + 1, x + m);
+        sort(y + 1, y + n);
+        reverse(y + 1, y + n);
+        int i = 1, j = 1;
+        int ans = 0;
+        while (i < m && j < n)
+        {
+            if (x[i] > y[j])
+            {
+                ans += x[i] * v;
+                ++h;
+                ++i;
+            }
+            else
+            {
+                ans += y[j] * h;
+                ++v;
+                ++j;
+            }
+        }
+        if (i < m)
+        {
+            int sum = 0;
+            while (i < m)
+            {
+                sum += x[i];
+                ++i;
+            }
+            ans += sum * v;
         }
         else
         {
-            ans += (Y[j] * h_cuts);
-            v_cuts++;
-            j++;
+            int sum = 0;
+            while (j < n)
+            {
+                sum += y[j];
+                ++j;
+            }
+            ans += sum * h;
         }
+        cout << ans << endl;
     }
-    while (i < m)
-    {
-        ans += (X[i] * v_cuts);
-        h_cuts++;
-        i++;
-    }
-    while (j < n)
-    {
-        ans += (Y[j] * h_cuts);
-        v_cuts++;
-        j++;
-    }
-    return ans;
-}
-
-// Driver code to test above methods
-int main()
-{
-    int T;
-    cin >> T;
-    while (T--)
-    {
-        int n, m;
-        cin >> m >> n;
-        int X[m - 1], Y[n - 1];
-        for (int i = 0; i < m - 1; i++)
-        {
-            cin >> X[i];
-        }
-        for (int i = 0; i < n - 1; i++)
-        {
-            cin >> Y[i];
-        }
-        cout << minimumCostOfBreaking(X, Y, m - 1, n - 1);
-    }
+    return 0;
 }

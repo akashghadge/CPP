f422d45b10cd218502fd550358f2eb9d39d4161f
akash_ghadge <asghadge6@gmail.com>
2021-11-10 18:19:13 +0530

    DSA Recursion and BT Adv PEP 1 added

---
+++ DSA Questions/Recursion/PEP Advance/Knight Kill Combination.cpp
@@ -0,0 +1,107 @@
+/*
+this question is same as perior queen P 1 but here we need to take care of killing of queen as well
+here we basically using the last cell technicqe
+same tech as queen kill combination expect isSafe function
+here we check any prior knight killing current one or not
+but here i check all ;)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+int solutions = 0;
+void print(vector<vector<int>> board)
+{
+    for (auto list : board)
+    {
+        for (auto val : list)
+        {
+            val ? cout << " q " : cout << " _ ";
+        }
+        cout << en;
+    }
+    cout << en;
+}
+bool isSafe(vector<vector<int>> &chess, int row, int col)
+{
+    int nr, nc;
+
+    nr = row - 2;
+    nc = col + 1;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    nr = row - 1;
+    nc = col + 2;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    nr = row + 1;
+    nc = col + 2;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    nr = row + 2;
+    nc = col + 1;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    nr = row + 2;
+    nc = col - 1;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    nr = row + 1;
+    nc = col - 2;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    nr = row - 1;
+    nc = col - 2;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    nr = row - 2;
+    nc = col - 1;
+    if (nr > 0 && nr < chess.size() && nc < chess.size() && nc > 0 && chess[nr][nc] != 0)
+        return false;
+
+    return true;
+}
+void sol(int curr, int total, vector<vector<int>> &chess, int cell)
+{
+    if (curr >= total)
+    {
+        cout << "Answers :" << en;
+        print(chess);
+        solutions++;
+        return;
+    }
+    for (int i = cell + 1; i < chess.size() * chess.size(); i++)
+    {
+        int row = i / chess.size();
+        int col = i % chess.size();
+        if (isSafe(chess, row, col) && chess[row][col] == 0)
+        {
+            chess[row][col] = 1;
+            sol(curr + 1, total, chess, row * chess.size() + col);
+            chess[row][col] = 0;
+        }
+    }
+}
+int main()
+{
+    FAST;
+    int N;
+    cin >> N;
+    vector<vector<int>> chess(N, vector<int>(N));
+    sol(0, N, chess, 0);
+    cout << "Total Solutions :" << solutions << en;
+    return 0;
+}

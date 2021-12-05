f4e487303db97a8ef15ecba7883cfa0bcdbff619
akash_ghadge <asghadge6@gmail.com>
2021-12-05 13:10:12 +0530

    DSA SHEET DYNAMIC PROGRAMMING IS STARTED'

---
+++ DSA crack sheet/DP/01 Coin Change.cpp
@@ -0,0 +1,69 @@
+/*
+Given a value N, find the number of ways to make change for N cents, if we have infinite supply of each of S = { S1, S2, .. , SM } valued coins.
+
+
+Example 1:
+
+Input:
+n = 4 , m = 3
+S[] = {1,2,3}
+Output: 4
+Explanation: Four Possible ways are:
+{1,1,1,1},{1,1,2},{2,2},{1,3}.
+Example 2:
+
+Input:
+n = 10 , m = 4
+S[] ={2,5,3,6}
+Output: 5
+Explanation: Five Possible ways are:
+{2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} 
+and {5,5}.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function count() which accepts an array S[] its size m and n as input parameter and returns the number of ways to make change for N cents.
+
+
+Expected Time Complexity: O(m*n).
+Expected Auxiliary Space: O(n).
+
+
+Constraints:
+1 <= n,m <= 103
+
+View Bookmarked Problems
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+long long int count(int coins[], int n, int k)
+{
+    // code here.
+    // int n = coins.size();
+    vector<long long> dp(k + 1);
+    dp[0] = 1;
+    for (int i = 0; i < n; i++)
+    {
+        int coin = coins[i];
+        for (int j = coin; j <= k; j++)
+        {
+            dp[j] += dp[j - coin];
+        }
+    }
+    return dp[k];
+}
+int main()
+{
+    FAST;
+
+    return 0;
+}

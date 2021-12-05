a932d585824658bf905a3124ae1627c9b8cfa7fe
akash_ghadge <asghadge6@gmail.com>
2021-12-05 12:15:18 +0530

    DSA dp pep adv 1 added

--- DSA Questions/DP/PEP Adv/00 Largest Rectangle.cpp
+++ DSA Questions/DP/PEP Adv/00 Largest Rectangle.cpp
@@ -27,7 +27,7 @@ int max_rectangle(vii &grid)
     vii dp(n, vi(m));
     int ans = INT_MIN;
     for (int i = n - 1; i >= 0; i--)
-    {
+    { 
         for (int j = m - 1; j >= 0; j--)
         {
             if (grid[i][j] == 0 || i == n - 1 || j == m - 1)

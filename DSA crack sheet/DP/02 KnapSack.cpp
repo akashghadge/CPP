b4a0ab2ae5961bc54503cad7c991b52423e40475
akash_ghadge <asghadge6@gmail.com>
2021-12-05 13:12:19 +0530

    DSA sheet dp 1 added

---
+++ DSA crack sheet/DP/02 KnapSack.cpp
@@ -0,0 +1,76 @@
+/*
+0 - 1 Knapsack Problem You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item or don’t pick it (0-1 property).
+
+Example 1:
+
+Input:
+N = 3
+W = 4
+values[] = {1,2,3}
+weight[] = {4,5,1}
+Output: 3
+Example 2:
+
+Input:
+N = 3
+W = 3
+values[] = {1,2,3}
+weight[] = {4,5,6}
+Output: 0
+Your Task:
+Complete the function knapSack() which takes maximum capacity W, weight array wt[], value array val[], and the number of items n as a parameter and returns the maximum possible value you can get.
+
+Expected Time Complexity: O(N*W).
+Expected Auxiliary Space: O(N*W)
+
+Constraints:
+1 ≤ N ≤ 1000
+1 ≤ W ≤ 1000
+1 ≤ wt[i] ≤ 1000
+1 ≤ v[i] ≤ 1000
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+public:
+//Function to return max value that can be put in knapsack of capacity W.
+int knapSack(int capicity, int weight[], int price[], int n)
+{
+    // Your code here
+    vector<vector<int>> dp(n + 1, vector<int>(capicity + 1));
+    for (int i = 1; i < dp.size(); i++)
+    {
+        for (int j = 1; j < dp[0].size(); j++)
+        {
+            if (j >= weight[i - 1])
+            {
+                int rem_weight = j - weight[i - 1];
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][rem_weight] + price[i - 1]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][capicity];
+}
+int main()
+{
+    FAST;
+
+    return 0;
+}

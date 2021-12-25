fbbbaba1afb5aed00c20bed6ff7e2b2f1c540356
akash_ghadge <asghadge6@gmail.com>
2021-12-25 22:22:33 +0530

    DSA dp pep adv 1 added

---
+++ DSA Questions/DP/PEP Adv/30 Count Palindrome Substrings.cpp
@@ -0,0 +1,68 @@
+/*
+string aabbaa
+find count of substrings which are palindrome
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+template <class printVal>
+void printVector(vector<printVal> vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (auto val : vec[i])
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+int count_palindrom_substring(string str)
+{
+    int n = str.size();
+    vii dp(n, vi(n, 0));
+    int ans = 0;
+    for (int gap = 0; gap < n; gap++)
+    {
+        for (int i = 0, j = gap; j < n; i++, j++)
+        {
+            if (gap == 0)
+            {
+                dp[i][j] = 1;
+            }
+            else if (gap == 1 && str[i] == str[j])
+            {
+                dp[i][j] = 1;
+            }
+            else if (str[i] == str[j] && dp[i + 1][j - 1])
+            {
+                dp[i][j] = 1;
+            }
+            else
+            {
+                dp[i][j] = 0;
+            }
+            if (dp[i][j])
+                ans++;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    FAST;
+    string str;
+    cin >> str;
+    cout << count_palindrom_substring(str) << en;
+    return 0;
+}

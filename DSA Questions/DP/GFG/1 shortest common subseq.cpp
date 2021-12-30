08669cc7600bf56c4b1c34b712ee63a2f617f83c
akash_ghadge <asghadge6@gmail.com>
2021-12-31 00:36:07 +0530

    DSA dp GFG 1 added

---
+++ DSA Questions/DP/GFG/1 shortest common subseq.cpp
@@ -0,0 +1,83 @@
+/*
+Given two strings X and Y of lengths m and n respectively, find the length of the smallest string which has both, X and Y as its sub-sequences.
+Note: X and Y can have both uppercase and lowercase letters.
+
+Example 1
+
+Input:
+X = abcd, Y = xycd
+Output: 6
+Explanation: Shortest Common Supersequence
+would be abxycd which is of length 6 and
+has both the strings as its subsequences.
+Example 2
+
+Input:
+X = efgh, Y = jghi
+Output: 6
+Explanation: Shortest Common Supersequence
+would be ejfghi which is of length 6 and
+has both the strings as its subsequences.
+Your Task:
+Complete shortestCommonSupersequence() function that takes X, Y, m, and n as arguments and returns the length of the required string.
+
+Expected Time Complexity: O(Length(X) * Length(Y)).
+Expected Auxiliary Space: O(Length(X) * Length(Y)).
+
+Constraints:
+1<= |X|, |Y| <= 100
+
+ 
+
+View Bookmarked Problems
+Company Tags
+Topic Tags
+Related Courses
+Related Interview Experiences
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+int shortestCommonSupersequence(string x, string y, int m, int n)
+{
+    //code here
+    int t[m + 1][n + 1];
+    for (int i = 0; i < m + 1; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
+        {
+            if (i == 0 || j == 0)
+                t[i][j] = 0;
+        }
+    }
+    for (int i = 1; i < m + 1; i++)
+    {
+        for (int j = 1; j < n + 1; j++)
+        {
+            if (x[i - 1] == y[j - 1])
+            {
+                t[i][j] = 1 + t[i - 1][j - 1];
+            }
+            else
+            {
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+            }
+        }
+    }
+    int x1 = t[m][n];
+
+    return m + n - (x1);
+}
+int main()
+{
+    FAST;
+
+    return 0;
+}

8611c1497f87986bd632b40e263a441a424ac7c8
akash_ghadge <asghadge6@gmail.com>
2022-01-08 18:55:52 +0530

    no change

--- DSA crack sheet/DP/31 Minimum removals from array to make max min K.cpp
+++ DSA crack sheet/DP/31 Minimum removals from array to make max min K.cpp
@@ -20,95 +20,112 @@ The other is to remove 1, 2, 5
 
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// CPP program to find minimum removals
-// to make max-min <= K
 #include <bits/stdc++.h>
 using namespace std;
-
-#define MAX 100
-int dp[MAX][MAX];
-
-// function to check all possible combinations
-// of removal and return the minimum one
-int countRemovals(int a[], int i, int j, int k)
+#define en "\n"
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vll vector<ll>
+#define ll long long
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+int util(vi arr, int n, int k, int i, int j, vii &dp)
 {
-	// base case when all elements are removed
-	if (i >= j)
-		return 0;
+    if (i >= j)
+        return n;
+    else if (arr[j] - arr[i] <= k)
+        return (n - j - 1 + i);
+    if (dp[i][j] != -1)
+        return dp[i][j];
 
-	// if condition is satisfied, no more
-	// removals are required
-	else if ((a[j] - a[i]) <= k)
-		return 0;
-
-	// if the state has already been visited
-	else if (dp[i][j] != -1)
-		return dp[i][j];
-
-	// when Amax-Amin>d
-	else if ((a[j] - a[i]) > k) {
-
-		// minimum is taken of the removal
-		// of minimum element or removal
-		// of the maximum element
-		dp[i][j] = 1 + min(countRemovals(a, i + 1, j, k),
-						countRemovals(a, i, j - 1, k));
-	}
-	return dp[i][j];
+    int ans1 = util(arr, n, k, i + 1, j, dp);
+    int ans2 = util(arr, n, k, i, j - 1, dp);
+    dp[i][j] = min(ans1, ans2);
+    return dp[i][j];
+}
+int sol(vi arr, int n, int k)
+{
+    sort(arr.begin(), arr.end());
+    vii dp(n, vi(n, -1));
+    return util(arr, n, k, 0, n - 1, dp);
 }
 
-// To sort the array and return the answer
-int removals(int a[], int n, int k)
-{
-	// sort the array
-	sort(a, a + n);
-
-	// fill all stated with -1
-	// when only one element
-	memset(dp, -1, sizeof(dp));
-	if (n == 1)
-		return 0;
-	else
-		return countRemovals(a, 0, n - 1, k);
-}
-
-// Driver Code
 int main()
 {
-	int a[] = { 1, 3, 4, 9, 10, 11, 12, 17, 20 };
-	int n = sizeof(a) / sizeof(a[0]);
-	int k = 4;
-	cout << removals(a, n, k);
-	return 0;
+    FAST;
+    int N;
+    cin >> N;
+    vi arr(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+    cout << sol(arr, N, k) << en;
+    return 0;
 }
+
+// // CPP program to find minimum removals
+// // to make max-min <= K
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define MAX 100
+// int dp[MAX][MAX];
+
+// // function to check all possible combinations
+// // of removal and return the minimum one
+// int countRemovals(int a[], int i, int j, int k)
+// {
+//     // base case when all elements are removed
+//     if (i >= j)
+//         return 0;
+
+//     // if condition is satisfied, no more
+//     // removals are required
+//     else if ((a[j] - a[i]) <= k)
+//         return 0;
+
+//     // if the state has already been visited
+//     else if (dp[i][j] != -1)
+//         return dp[i][j];
+
+//     // when Amax-Amin>d
+//     else if ((a[j] - a[i]) > k)
+//     {
+
+//         // minimum is taken of the removal
+//         // of minimum element or removal
+//         // of the maximum element
+//         dp[i][j] = 1 + min(countRemovals(a, i + 1, j, k),
+//                            countRemovals(a, i, j - 1, k));
+//     }
+//     return dp[i][j];
+// }
+
+// // To sort the array and return the answer
+// int removals(int a[], int n, int k)
+// {
+//     // sort the array
+//     sort(a, a + n);
+
+//     // fill all stated with -1
+//     // when only one element
+//     memset(dp, -1, sizeof(dp));
+//     if (n == 1)
+//         return 0;
+//     else
+//         return countRemovals(a, 0, n - 1, k);
+// }
+
+// // Driver Code
+// int main()
+// {
+//     int a[] = {1, 3, 4, 9, 10, 11, 12, 17, 20};
+//     int n = sizeof(a) / sizeof(a[0]);
+//     int k = 4;
+//     cout << removals(a, n, k);
+//     return 0;
+// }

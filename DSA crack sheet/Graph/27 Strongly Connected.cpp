3b4d87bc14f41677e4b97d53aae39b0732ba0821
akash_ghadge <asghadge6@gmail.com>
2022-02-16 10:31:04 +0530

    DSA sheet graph 1 added

---
+++ DSA crack sheet/Graph/27 Strongly Connected.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+int strongly_connected(vector<int> adj[], int V)
+{
+    
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+
+    return 0;
+}
---
+++ DSA crack sheet/Graph/37 M coloring.cpp
@@ -0,0 +1,107 @@
+/*
+Given an undirected graph and an integer M. The task is to determine if the graph can be colored with at most M colors such that no two adjacent vertices of the graph are colored with the same color. Here coloring of a graph means the assignment of colors to all vertices. Print 1 if it is possible to colour vertices and 0 otherwise.
+
+Example 1:
+
+Input:
+N = 4
+M = 3
+E = 5
+Edges[] = {(0,1),(1,2),(2,3),(3,0),(0,2)}
+Output: 1
+Explanation: It is possible to colour the
+given graph using 3 colours.
+Example 2:
+
+Input:
+N = 3
+M = 2
+E = 3
+Edges[] = {(0,1),(1,2),(0,2)}
+Output: 0
+Your Task:
+Your task is to complete the function graphColoring() which takes the 2d-array graph[], the number of colours and the number of nodes as inputs and returns true if answer exists otherwise false. 1 is printed if the returned value is true, 0 otherwise. The printing is done by the driver's code.
+Note: In Example there are Edges not the graph.Graph will be like, if there is an edge between vertex X and vertex Y graph[] will contain 1 at graph[X-1][Y-1], else 0. In 2d-array graph[ ], nodes are 0-based indexed, i.e. from 0 to N-1.Function will be contain 2-D graph not the edges.
+
+Expected Time Complexity: O(MN).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 ≤ N ≤ 20
+1 ≤ E ≤ (N*(N-1))/2
+1 ≤ M ≤ N
+
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+vector<vector<int>> convert(bool a[101][101], int V)
+{
+    vector<vector<int>> adjList(V);
+    for (int i = 0; i < V; i++)
+    {
+
+        for (int j = 0; j < V; j++)
+        {
+            if (a[i][j] == 1)
+            {
+                adjList[i].push_back(j);
+            }
+        }
+    }
+    return adjList;
+}
+bool safe(vector<vector<int>> &graph, int src, int src_color, int V, vector<int> &colors)
+{
+    for (auto nbr : graph[src])
+    {
+        if (colors[nbr] == src_color)
+            return false;
+    }
+    return true;
+}
+bool sol(int curr, vector<vector<int>> &graph, int m, int V, vector<int> &colors)
+{
+    if (curr == V)
+        return true;
+    for (int i = 1; i <= m; i++)
+    {
+        if (safe(graph, curr, i, V, colors))
+        {
+            colors[curr] = i;
+            if (sol(curr + 1, graph, m, V, colors))
+                return true;
+            colors[curr] = 0;
+        }
+    }
+
+    return false;
+}
+bool graphColoring(bool graph[101][101], int m, int V)
+{
+    // your code here
+    vector<vector<int>> g = convert(graph, V);
+    vector<int> colors(V, 0);
+    return sol(0, g, m, V, colors);
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+
+    return 0;
+}

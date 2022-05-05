3779a389fb2b12743ea94dd8e46af599db84c230
akash_ghadge <asghadge6@gmail.com>
2022-05-05 23:27:29 +0530

    No Change

--- temp.cpp
+++ temp.cpp
@@ -1,22 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define en "\n"
-#define vi vector<int>
-#define vii vector<vector<int>>
-#define vll vector<ll>
-#define ll long long
-#define FAST                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
+#include <stdio.h>
+float add(int, int);
 int main()
 {
-    FAST;
-    int res = 1;
-    for (int i = 0; i < 10; i++)
-    {
-        res = res << 1;
-    }
-    cout << res;
-
-    return 0;
+    int num[4] = {1, 3, 4, 5};
+    int *ptr = num;
+    ptr++;
+    printf("%d", *ptr);
 }

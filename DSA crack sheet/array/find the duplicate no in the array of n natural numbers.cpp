a6607b476200d2274b9c78ca4d77c139a19a0387
akash_ghadge <asghadge6@gmail.com>
2023-05-02 11:21:31 +0530

    DSA Array Revision

--- DSA crack sheet/array/find the duplicate no in the array of n natural numbers.cpp
+++ DSA crack sheet/array/find the duplicate no in the array of n natural numbers.cpp
@@ -1,37 +1,41 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 // here so many approches are possible
 // use two loops and find dup in o(n^2)
 // use sort and o(n) for all elements O(nlogn)
 // use map o(n) and space o(n)
 // use current arrray as map due to numbers from 1 to n+1
-int duplicate(int *, int);
-void quickSort(int *, int, int);
-int partition(int *, int, int);
-void swap_element(int *a, int *b)
+
+/*
+most optimized
+Since all values of the array are between [1…n][1 \dots n][1…n] and the array size is n+1n+1n+1, while scanning the array from left to right, we set the nums[n]\textit{nums}[n]nums[n] to its negative value.
+With extra O(1)O(1)O(1) space, with modifying the input.
+*/
+int findDuplicate_mark(int nums[], int len)
 {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+    for (int i = 0; i < len; i++)
+    {
+        int num = nums[i];
+        int idx = abs(num);
+        if (nums[idx] < 0)
+        {
+            return idx;
+        }
+        nums[idx] = -nums[idx];
+    }
+    return len;
 }
+
+int duplicate(int *, int);
 void setArray(int *arr, int size)
 {
-    int i;
-    for (i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++)
         cin >> arr[i];
-    }
-    cout << endl;
 }
 void printArray(int *arr, int size)
 {
-    int i;
-    for (i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++)
         cout << arr[i] << endl;
-    }
-    cout << endl;
 }
 int main()
 {
@@ -44,51 +48,14 @@ int main()
         int arr[size];
         setArray(arr, size);
         printArray(arr, size);
-        quickSort(arr, 0, size);
-        printArray(arr, size);
         int result = duplicate(arr, size);
         cout << "Result : " << result << endl;
     }
 }
 
-void quickSort(int arr[], int lb, int ub)
-{
-    int loc;
-    if (lb < ub)
-    {
-        loc = partition(arr, lb, ub);
-        quickSort(arr, lb, loc - 1);
-        quickSort(arr, loc + 1, ub);
-    }
-}
-
-int partition(int arr[], int lb, int ub)
-{
-    int pivot;
-    int start = lb;
-    int end = ub;
-    pivot = arr[lb];
-    while (start < end)
-    {
-        while (arr[start] <= pivot)
-        {
-            start++;
-        }
-        while (arr[end] > pivot)
-        {
-            end--;
-        }
-        if (start < end)
-        {
-            swap_element(&arr[start], &arr[end]);
-        }
-    }
-    swap_element(&arr[lb], &arr[end]);
-    return end;
-}
-
 int duplicate(int *arr, int size)
 {
+    sort(arr, arr + size);
     int i;
     for (i = 0; i < size - 1; i++)
     {
